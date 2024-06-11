8430ffe736a9ed745f0ba16d4367d8e566f5b358
HCR-750F <54973190+sch-lda@users.noreply.github.com>
2024-06-11 08:02:30 +0800

    opt

--- src/hooks/protections/receive_net_message.cpp
+++ src/hooks/protections/receive_net_message.cpp
@@ -143,7 +143,7 @@ namespace big
 					if (g.session.log_chat_messages)
 						chat::log_chat(message, player, spam_reason, is_team);
 
-					if ((g.session.spam_timer > 2.0f && g.session.spam_length > 20 && g.session.auto_report_spam) || spam_reason == SpamReason::STATIC_DETECTION)
+					if ((g.session.spam_timer <= 5.0f && g.session.spam_length > 20 && g.session.auto_report_spam) || spam_reason == SpamReason::STATIC_DETECTION)
 					{
 						g_thread_pool->push([message, player, spam_reason] {
 							bool isok = g_api_service->report_spam(message, player->get_rockstar_id(), spam_reason);
