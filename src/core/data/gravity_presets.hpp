7ccc1ff2d722f206b0f46bb59ee731ff3b3a49b7
HCR-750F <54973190+sch-lda@users.noreply.github.com>
2024-07-27 23:37:07 +0800

    gravity translation

--- src/core/data/gravity_presets.hpp
+++ src/core/data/gravity_presets.hpp
@@ -2,33 +2,24 @@
 
 namespace big
 {
-	constexpr const static auto gravity_presets = std::to_array({
-		"Moon", 
-		"Sun", 
-		"Pluto", 
-		"Space", 
-		"Mercury", 
-		"Venus",
-		"Earth",
-		"Mars", 
-		"Jupiter", 
-		"Saturn", 
-		"Uranus", 
-		"Neptune"
-		});
+	struct gravitytype
+	{
+		const char* name;
+		float value;
+	};
 
-	constexpr const static auto gravity_preset_values = std::to_array({
-		1.6f,
-		274.f,
-		0.6f,
-		0.f,
-		3.7f,
-		8.9f,
-		9.8f,
-		3.7f,
-		24.8f,
-		10.5f,
-		8.7f,
-		11.2f
-		});
+	constexpr const static auto gravity_presets = std::to_array<gravitytype>({
+		{"GRAVITY_MOON", 1.6f},
+		{"GRAVITY_SUN", 274.f},
+		{"GRAVITY_PLUTO", 0.6f},
+		{"GRAVITY_SPACE", 0.f},
+		{"GRAVITY_MERCURY", 3.7f},
+		{"GRAVITY_VENUS", 8.9f},
+		{"GRAVITY_EARTH", 9.8f},
+		{"GRAVITY_MARS", 3.7f},
+		{"GRAVITY_JUPITER", 24.8f},
+		{"GRAVITY_SATURN", 10.5f},
+		{"GRAVITY_URANUS", 8.7f},
+		{"GRAVITY_NEPTUNE", 11.2f}
+	});
 }
--- src/views/world/view_gravity.cpp
+++ src/views/world/view_gravity.cpp
@@ -12,10 +12,21 @@ namespace big
 			ImGui::SliderFloat("GRAVITY_LEVEL"_T.data(), &g.world.gravity.current_gravity, 0.f, 1000.f, "%.1f");
 
 			static int selected_lunar_preset = 0;
-
-			if (ImGui::Combo("GRAVITY_LUNAR_PRESETS"_T.data(), &selected_lunar_preset, gravity_presets.data(), gravity_presets.size()))
+			if (ImGui::BeginCombo("GRAVITY_LUNAR_PRESETS"_T.data(),
+			        g_translation_service.get_translation(gravity_presets[selected_lunar_preset].name).data()))
 			{
-				g.world.gravity.current_gravity = gravity_preset_values[selected_lunar_preset];
+				for (int i = 0; i < gravity_presets.size(); i++)
+				{
+					if (ImGui::Selectable(
+					        g_translation_service.get_translation(gravity_presets[i].name).data(),
+					        g.world.local_weather == i))
+					{
+						g.world.gravity.current_gravity = gravity_presets[i].value;
+						selected_lunar_preset = i;
+					}
+				}
+
+				ImGui::EndCombo();
 			}
 		}
 	}
